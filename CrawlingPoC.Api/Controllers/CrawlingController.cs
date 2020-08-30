a254283f0b57903874eb3df0c6cdae11f6523a14
Enio Oliveira <eonascimetno@latam.stefanini.com>
2020-08-29 23:08:57 -0300

    Example with the  AngleSharp library

--- CrawlingPoC.Api/Controllers/CrawlingController.cs
+++ CrawlingPoC.Api/Controllers/CrawlingController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using AngleSharp;
+using CrawlingPoC.Api.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,10 +15,16 @@ namespace CrawlingPoC.Api.Controllers
     public class CrawlingController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<List<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            var config = Configuration.Default.WithDefaultLoader();
+            var address = "https://en.wikipedia.org/wiki/List_of_The_Big_Bang_Theory_episodes";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(address);
+            var cellSelector = "tr.vevent td:nth-child(3)";
+            var cells = document.QuerySelectorAll(cellSelector);
+            var titles = cells.Select(m => m.TextContent).ToList();
+            return titles;
         }
-
     }
 }
---
+++ CrawlingPoC.Api/ViewModels/SharesResultViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrawlingPoC.Api.ViewModels
+{
+    public class SharesResultViewModel
+    {
+        public SharesResultViewModel()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public string AcronymShare { get; set; }
+        public string NameShare { get; set; }
+    }
+}
